c74006320743830e0dcd4d3ff9064d01c5882ffc
CrossVas <cross.wanted.1.4@gmail.com>
2024-07-03 08:57:03 +0900

    added chance label for basic machines

--- src/main/java/dev/crossvas/ic2rei/categories/BaseMachineCategory.java
+++ src/main/java/dev/crossvas/ic2rei/categories/BaseMachineCategory.java
@@ -4,12 +4,13 @@ import com.google.common.collect.Lists;
 import dev.crossvas.ic2rei.displays.BaseMachineDisplay;
 import dev.crossvas.ic2rei.utils.GuiHelper;
 import dev.crossvas.ic2rei.utils.IGuiHelper;
-import ic2.core.inventory.gui.IC2Screen;
+import ic2.core.utils.helpers.Formatters;
+import me.shedaniel.math.Dimension;
 import me.shedaniel.math.Rectangle;
 import me.shedaniel.rei.api.client.gui.Renderer;
 import me.shedaniel.rei.api.client.gui.widgets.Widget;
+import me.shedaniel.rei.api.client.gui.widgets.Widgets;
 import me.shedaniel.rei.api.client.registry.display.DisplayCategory;
-import me.shedaniel.rei.api.client.registry.screen.ExclusionZonesProvider;
 import me.shedaniel.rei.api.common.category.CategoryIdentifier;
 import me.shedaniel.rei.api.common.entry.EntryIngredient;
 import me.shedaniel.rei.api.common.util.EntryIngredients;
@@ -17,10 +18,9 @@ import me.shedaniel.rei.api.common.util.EntryStacks;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.level.block.Block;
 
-import java.util.Collection;
 import java.util.List;
 
-public class BaseMachineCategory implements DisplayCategory<BaseMachineDisplay>, IGuiHelper, ExclusionZonesProvider<IC2Screen> {
+public class BaseMachineCategory implements DisplayCategory<BaseMachineDisplay>, IGuiHelper {
 
     protected Block BLOCK;
     protected CategoryIdentifier<? extends BaseMachineDisplay> ID;
@@ -52,9 +52,17 @@ public class BaseMachineCategory implements DisplayCategory<BaseMachineDisplay>,
         List<Widget> widgets = Lists.newArrayList();
         EntryIngredient input = EntryIngredients.ofIngredient(display.getRecipe().getInput());
         GuiHelper.createRecipeBase(widgets, bounds);
+        if (display.getRecipe().getChance() > 0) {
+            Component text = Component.literal(Formatters.XP_FORMAT.format(display.getRecipe().getChance() * 100.0F) + "%");
+            GuiHelper.addLabel(widgets, point(bounds.getCenterX(), bounds.getMaxY() - 5 - lineHeight), text);
+            int width = font.width(text.getString());
+            widgets.add(Widgets.createTooltip(new Rectangle(point(bounds.getCenterX() - width / 2, bounds.getMaxY() - 5 - lineHeight), new Dimension(width, lineHeight)), format("jei.ic2.recycler.chance")));
+        }
         GuiHelper.addProgressBar(widgets, point(getProgressBarX(bounds), getProgressBarY(bounds)), 3000, this.TYPE);
         GuiHelper.addInputSlot(widgets, adjustedInputPoint(bounds), input);
         GuiHelper.addLargeOutputSlot(widgets, adjustedOutputPoint(bounds), EntryIngredients.ofItemStacks(display.getRecipe().getOutputs()));
+
+
         return widgets;
     }
 
@@ -66,10 +74,5 @@ public class BaseMachineCategory implements DisplayCategory<BaseMachineDisplay>,
     @Override
     public int getDisplayHeight() {
         return getHeight();
-    }
-
-    @Override
-    public Collection<Rectangle> provide(IC2Screen screen) {
-        return null;
     }
 }
--- src/main/java/dev/crossvas/ic2rei/recipes/BaseMachineRecipe.java
+++ src/main/java/dev/crossvas/ic2rei/recipes/BaseMachineRecipe.java
@@ -1,5 +1,7 @@
 package dev.crossvas.ic2rei.recipes;
 
+import ic2.api.recipes.ingridients.recipes.IRecipeOutput;
+import ic2.api.recipes.ingridients.recipes.IRecipeOutputChance;
 import ic2.api.recipes.registries.IMachineRecipeList;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
@@ -10,10 +12,16 @@ public class BaseMachineRecipe {
 
     private final Ingredient INPUT;
     private final List<ItemStack> OUTPUTS;
+    float CHANCE;
+
 
     public BaseMachineRecipe(IMachineRecipeList.RecipeEntry recipeEntry) {
         this.INPUT = recipeEntry.getInputs()[0].asIngredient();
         this.OUTPUTS = recipeEntry.getOutput().getAllOutputs();
+        IRecipeOutput output = recipeEntry.getOutput();
+        if (output instanceof IRecipeOutputChance chance) {
+            CHANCE = chance.getChance();
+        }
     }
 
     public Ingredient getInput() {
@@ -24,5 +32,7 @@ public class BaseMachineRecipe {
         return this.OUTPUTS;
     }
 
-
+    public float getChance() {
+        return this.CHANCE;
+    }
 }
