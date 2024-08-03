23ddfd57e045b4a8922412899667f613be5f75ce
CrossVas <cross.wanted.1.4@gmail.com>
2024-08-03 10:27:06 +0900

    better filtering for quantum armor recipes based on specific tags

--- src/main/java/dev/crossvas/ic2rei/IC2REIPluginCommon.java
+++ src/main/java/dev/crossvas/ic2rei/IC2REIPluginCommon.java
@@ -1,18 +1,32 @@
 package dev.crossvas.ic2rei;
 
 import ic2.core.platform.registries.IC2Items;
+import me.shedaniel.rei.api.common.entry.comparison.EntryComparator;
 import me.shedaniel.rei.api.common.entry.comparison.ItemComparatorRegistry;
 import me.shedaniel.rei.api.common.plugins.REIServerPlugin;
 import me.shedaniel.rei.forge.REIPluginCommon;
+import me.shedaniel.rei.impl.Internals;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.Tag;
+import net.minecraft.world.item.ItemStack;
 
+@SuppressWarnings("UnstableApiUsage")
 @REIPluginCommon
 public class IC2REIPluginCommon implements REIServerPlugin {
 
     @Override
     public void registerItemComparators(ItemComparatorRegistry registry) {
-        registry.registerNbt(IC2Items.QUANTUM_SUIT_HELMET,
+        registry.register(subTag("display", "color"), IC2Items.QUANTUM_SUIT_HELMET,
                 IC2Items.QUANTUM_SUIT_CHESTPLATE,
                 IC2Items.QUANTUM_SUIT_LEGGINGS,
                 IC2Items.QUANTUM_SUIT_BOOTS);
+    }
+
+    public static EntryComparator<ItemStack> subTag(String tag, String subTag) {
+        EntryComparator<Tag> hasher = Internals.getNbtHasher(new String[]{tag});
+        return (comparisonContext, stack) -> {
+            CompoundTag stackTag = stack.getOrCreateTag().getCompound(tag);
+            return !stackTag.contains(subTag) ? 0 : hasher.hash(comparisonContext, stackTag);
+        };
     }
 }
